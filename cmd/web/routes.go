53792e41c7ef5cdde911a2a6b3b7ac83f6e00146
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-07 14:15:16 +0200

    Add CSRFToken data & Post handlers
    -Added necessary CSRF Tokens for search-availability page
    -Added a handler to handle search-availability post request

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,6 +21,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/nebula", handlers.Repo.Nebula)
 	mux.Get("/darkfathom", handlers.Repo.Darkfathom)
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
