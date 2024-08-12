d4dba7cf5649799401693ebc463966e0433037d1
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-12 16:48:09 +0200

    Add render tests
    -Start working on tests for render pkg

---
+++ internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/LucasWiman90/GoWebApp/internal/config"
+	"github.com/LucasWiman90/GoWebApp/internal/models"
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{})
+	//Change this to true when in production
+	testApp.InProduction = false
+
+	//Setup sessions
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
