bbe7abfd257e32570f36204a2db636298d78a3f9
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-21 16:46:51 +0200

    Update JavaScript for individual rooms
    -Update jsonResponse structs
    -When a room is available show a button that allows the user
    to continue the booking process. The button holds a link
    based on the information entered

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -206,8 +206,11 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 }
 
 type jsonResponse struct {
-	OK      bool   `json:"ok"`
-	Message string `json:"message"`
+	OK        bool   `json:"ok"`
+	Message   string `json:"message"`
+	RoomID    string `json:"room_id"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
 }
 
 // AvailabilityJSON handles request for availability and sends JSON response
@@ -241,7 +244,11 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp := jsonResponse{
-		OK: available,
+		OK:        available,
+		Message:   "",
+		StartDate: sd,
+		EndDate:   ed,
+		RoomID:    strconv.Itoa(roomID),
 	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
