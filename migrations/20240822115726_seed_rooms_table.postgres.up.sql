081453a8ffc27d7d0d652f477522b508c9e2cc78
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-22 14:03:42 +0200

    Add migrations for seeding the database
    -Seeding rooms
    -Seeding restrictions

---
+++ migrations/20240822115726_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Nebula Quarters','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+	 ('Darkfathom Suite','2024-08-20 00:00:00.000','2024-08-20 00:00:00.000');
