afc59df0b2fad5f585f7a69d76deed4bfb7cb6ba
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-26 16:05:28 +0200

    Fix comment in handlers test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -86,7 +86,7 @@ func TestRepository_Reservation(t *testing.T) {
 		t.Errorf("Reservation handler returned wrong response code: god %d, wanted %d", rr.Code, http.StatusOK)
 	}
 
-	//test case with non-existant room
+	//test case with non-existant room, roomID that doesnt exist
 	req, _ = http.NewRequest("GET", "/make-reservation", nil)
 	ctx = getCtx(req)
 	req = req.WithContext(ctx)
