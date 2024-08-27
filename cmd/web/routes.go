4860b2c0681391f19b855d605842be23cf276697
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-27 12:07:42 +0200

    Create dropdown menu for admin
    -Create a dropdown menu for the admin

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -38,6 +38,7 @@ func routes(app *config.AppConfig) http.Handler {
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+	//Create subroutes for the admin
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(Auth)
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
