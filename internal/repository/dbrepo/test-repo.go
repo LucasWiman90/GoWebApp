0c08e04a4d3af37cc3a15463ad23035771766088
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-28 09:15:57 +0200

    Add SQL functions for updating/deleting a reservation
    -Add queries for getting a reservation by ID, updating
    a reservation, deleting and marking them as processed

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -139,3 +139,18 @@ func (m *testDBRepo) GetReservationById(id int) (models.Reservation, error) {
 
 	return res, nil
 }
+
+// UpdateReservation updates a reservation in the database
+func (m *testDBRepo) UpdateReservation(u models.Reservation) error {
+	return nil
+}
+
+// DeleteReservation deletes one reservation by id
+func (m *testDBRepo) DeleteReservation(id int) error {
+	return nil
+}
+
+// UpdateProcessedReservation updates processed for a reservation by id
+func (m *testDBRepo) UpdateProcessedForReservations(id, processed int) error {
+	return nil
+}
