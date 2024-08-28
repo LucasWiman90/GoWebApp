b318a04ff1b3a8def996b1e6039ff9e23cc6f14f
LucasWiman90 <lucas.wiman33@gmail.com>
2024-08-28 13:58:27 +0200

    Start working on reservation calendar
    -Calendar for the admin user to work with so he can clearly
    see reservations.
    -Added migrations for admin user
    -Added handler logic for calendar

---
+++ migrations/20240828115415_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Admin',E'User',E'admin@admin.com',E'$12$SkaltvFSefxLWhptWDcuweALVqbBj0dirgTvEsnSXad1sRTWiMeDm',3,E'2024-08-28 00:00:00',E'2024-08-28 00:00:00');
